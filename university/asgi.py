c549fe055630671c77bcd0899929fd4ade359acb
sajadmirzaee <41946561+sajadmirzaee@users.noreply.github.com>
2023-06-30 13:41:10 +0330

    Update asgi.py

--- university/asgi.py
+++ university/asgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+//nastarn
+
 import os
 
 from django.core.asgi import get_asgi_application
